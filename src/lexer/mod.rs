f2800e790b486f24e0451fadf8dfba2954a4b02c
endorpersand <daendrdragon@gmail.com>
2022-09-13 19:22:55 -0400

    impl full keyword & numeric parsing

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,75 +1,10 @@
 use std::collections::VecDeque;
 
+use self::token::{Token, Keyword};
+pub mod token;
+
 pub fn tokenize(input: &str) -> Result<Vec<Token>, LexErr> {
-    Lexer::new(input).lex()
-}
-
-pub enum Token {
-    Ident(String),
-    Numeric(String),
-    Str(String),
-    Comment(String),
-    
-    // keywords
-    Let,   // variable declarations
-    Const, // variable declarations
-    Mut,   // mutable variables and parameters
-    While, 
-    For, 
-    If, 
-    Else, 
-    Fun,   // functions
-    Shape, // shape X {}
-    Class, // class X {}
-    Fit,   // fit X to Y {}
-    To, 
-    Fits,    // class X fits Y {}
-    Extends, // shape X extends Y {}
-    Match,   // match x {}
-    Unit,    // units and measures (nominal primitives)
-    Measure, // units and measures (nominal primitives)
-    Of,      // units and measures (nominal primitives)
-
-    // operators
-    Plus,        // +
-    Minus,       // -
-    Star,        // *
-    Slash,       // /
-    Perc,        // %
-    
-    Dot,   // .
-    DDot,  // ..
-    Or,    // |
-    And,   // &
-    Tilde, // ~
-    Caret, // ^
-
-    DAnd,  // &&
-    DOr,   // ||
-    Exc,   // !
-
-    Lt,     // <
-    Le,     // <=
-    Gt,     // >
-    Ge,     // >=
-    Equal,  // =
-    DEqual, // ==
-    Ne, // !=
-
-    Shl, // <<
-    Shr, // >>
-
-    Semi,   // ;
-    Comma,  // ,
-    DSlash, // //
-
-    Hash,   // #
-
-    // delimiters
-    LParen,   RParen,   // ()
-    LSquare,  RSquare,  // []
-    LCurly,   RCurly,   // {}
-    LComment, RComment, // /* */
+    Lexer::new(input)?.lex()
 }
 
 pub enum LexErr {
@@ -78,7 +13,7 @@ pub enum LexErr {
 }
 
 struct Lexer {
-    input: VecDeque<char>,
+    input: VecDeque<CharData>,
     tokens: Vec<Token>
 }
 
@@ -93,6 +28,12 @@ enum CharClass {
     Whitespace
 }
 
+#[derive(PartialEq, Eq)]
+struct CharData {
+    chr: char,
+    cls: CharClass
+}
+
 impl CharClass {
     fn of(c: char) -> Option<Self> {
         if c.is_alphabetic()             { Some(Self::Alpha) }
@@ -104,33 +45,75 @@ impl CharClass {
         else if c.is_whitespace()        { Some(Self::Whitespace) }
         else { None }
     }
+}
 
+impl CharData {
+    fn new(c: char) -> Result<Self, LexErr> {
+        CharClass::of(c)
+            .ok_or(LexErr::UnrecognizedChar(c))
+            .map(|cls| Self { chr: c, cls })
+    }
 }
 
 impl Lexer {
-    fn new(input: &str) -> Self {
-        Lexer { input: input.chars().collect(), tokens: vec![] }
+    fn new(input: &str) -> Result<Self, LexErr> {
+        let cd = input.chars()
+            .map(CharData::new)
+            .collect::<Result<_, _>>()?;
+
+        Ok(Self {
+            input: cd, 
+            tokens: vec![]
+        })
     }
 
     /// Return the first non-consumed character in the input.
     /// 
     /// If there is no more characters in the input, return None.
-    fn peek(&self) -> Option<&char> {
+    fn peek(&self) -> Option<&CharData> {
         self.input.get(0)
     }
 
+    /// Return the next character and consume it.
+    fn next(&mut self) -> Option<CharData> {
+        self.input.pop_front()
+    }
+
+    /// Check if the character class matches. 
+    /// 
+    /// If yes, pop it and return the char.
+    /// If not, return None.
+    fn match_cls(&mut self, match_cls: CharClass) -> Option<char> {
+        match self.peek() {
+            Some(CharData { cls, .. }) if cls == &match_cls => {
+                self.next().map(|cd| cd.chr)
+            }
+            _ => None
+        }
+    }
+
+    /// Check if the character matches. 
+    /// 
+    /// If yes, pop it and return the char.
+    /// If not, return None.
+    fn match_chr(&mut self, match_chr: char) -> Option<char> {
+        match self.peek() {
+            Some(CharData { chr, .. }) if chr == &match_chr => {
+                self.next().map(|cd| cd.chr)
+            }
+            _ => None
+        }
+    }
+
     fn lex(mut self) -> Result<Vec<Token>, LexErr> {
-        while let Some(chr) = self.peek() {
-            let cls = CharClass::of(*chr).ok_or(LexErr::UnrecognizedChar(*chr))?;
+        while let Some(CharData { cls, .. }) = self.peek() {
             match cls {
                 CharClass::Alpha | CharClass::Underscore => self.push_ident(),
                 CharClass::Numeric    => self.push_numeric(),
                 CharClass::Quote      => self.push_str()?,
                 CharClass::Punct      => todo!(),
                 CharClass::NewLine    => todo!(),
-                CharClass::Whitespace => {
-                    self.input.pop_front();
-                },
+                CharClass::Whitespace => { self.next(); },
             }
         }
         
@@ -138,43 +121,71 @@ impl Lexer {
     }
 
     fn push_ident(&mut self) {
-        let c = self.input.pop_front().expect("String was validated to have a character, but failed to pop identifier");
-        let mut buf = String::from(c);
+        let cd = self.next()
+            .expect("String was validated to have a character, but failed to pop identifier");
+        let mut buf = String::from(cd.chr);
 
-        while let Some(c) = self.peek() {
-            match CharClass::of(*c).expect("String was validated to have a valid identifier character, but got None") {
+        while let Some(CharData { chr, cls }) = self.peek() {
+            match cls {
                 CharClass::Alpha | CharClass::Underscore | CharClass::Numeric => {
-                    buf.push(*c);
-                    self.input.pop_front();
+                    buf.push(*chr);
+                    self.next();
                 }
                 _ => break
             }
         }
 
-        // todo, deal with keywords
-        self.tokens.push(Token::Ident(buf));
-    }
-    fn push_numeric(&mut self) {
-        let c = self.input.pop_front().expect("String was validated to have a character, but failed to pop numeric");
-        let mut buf = String::from(c);
+        let token = Keyword::get_kw(&buf)
+            .unwrap_or_else(|| Token::Ident(buf));
 
-        while let Some(c) = self.peek() {
-            let cls = CharClass::of(*c).expect("String was validated to have a valid identifier character, but got None");
-            if cls == CharClass::Numeric {
-                buf.push(*c);
-                self.input.pop_front();
-            }
+        self.tokens.push(token);
+    }
+
+    fn push_numeric(&mut self) {
+        let cd = self.next()
+            .expect("String was validated to have a character, but failed to pop numeric");
+        let mut buf = String::from(cd.chr);
+
+        while let Some(c) = self.match_cls(CharClass::Numeric) {
+            buf.push(c);
         }
 
-        // TODO, deal with decimals vs range
+        // 123.ident  => [123][.][ident]
+        // 123.444    => [123.444]
+        // 123..444   => [123][..][444]
+        // 123. + 444 => [123.] [+] [444]
+        
+        // peek next character. check if it's .
+        if matches!(self.peek(), Some(CharData { chr: '.', .. })) {
+            // determine if this "." is part of the number or if it's part of a spread/call or such
+            match self.input.get(1) {
+                Some(CharData { chr: '.', ..}) => {},
+
+                // only consider if it is an operator or numeric after the .
+                Some(CharData { cls: CharClass::Numeric | CharClass::Punct, .. }) => {
+                    buf.push(self.next().unwrap().chr); // "."
+
+                    while let Some(c) = self.match_cls(CharClass::Numeric) {
+                        buf.push(c);
+                    }
+                }
+
+                _ => {}
+            }
+        }
         self.tokens.push(Token::Numeric(buf));
     }
 
     fn push_str(&mut self) -> Result<(), LexErr> {
-        let qt = self.input.pop_front().expect("String was validated to have a character, but failed to pop quotation mark");
+        let qt = self.next()
+            .expect("String was validated to have a character, but failed to pop quotation mark")
+            .chr;
+
         let mut buf = String::new();
         loop {
-            match self.input.pop_front() {
+            let c = self.next().map(|cd| cd.chr);
+
+            match c {
                 // hit quote? stop parsing string.
                 Some(c) if c == qt => break,
 
---
+++ src/lexer/token.rs
@@ -0,0 +1,90 @@
+pub enum Token {
+    Ident(String),
+    Numeric(String),
+    Str(String),
+    Comment(String),
+    Keyword(Keyword),
+
+    // operators
+    Plus,        // +
+    Minus,       // -
+    Star,        // *
+    Slash,       // /
+    Perc,        // %
+    
+    Dot,   // .
+    DDot,  // ..
+    Or,    // |
+    And,   // &
+    Tilde, // ~
+    Caret, // ^
+
+    DAnd,  // &&
+    DOr,   // ||
+    Exc,   // !
+
+    Lt,     // <
+    Le,     // <=
+    Gt,     // >
+    Ge,     // >=
+    Equal,  // =
+    DEqual, // ==
+    Ne, // !=
+
+    Shl, // <<
+    Shr, // >>
+
+    Semi,   // ;
+    Comma,  // ,
+    DSlash, // //
+
+    Hash,   // #
+
+    // delimiters
+    LParen,   RParen,   // ()
+    LSquare,  RSquare,  // []
+    LCurly,   RCurly,   // {}
+    LComment, RComment, // /* */
+}
+
+macro_rules! keywords {
+    ($($id:ident: $ex:expr),*) => {
+        pub enum Keyword {
+            $(
+                $id
+            ),+
+        }
+
+        impl Keyword {
+            pub fn get_kw(s: &str) -> Option<Token> {
+                match s {
+                    $(
+                        $ex => Some(Token::Keyword(Self::$id))
+                    ),+ ,
+                    _ => None
+                }
+            }
+        }
+    };
+}
+
+keywords! {
+    Let:     "let",   // variable declarations
+    Const:   "const", // variable declarations
+    Mut:     "mut",   // mutable variables and parameters
+    While:   "while", 
+    For:     "for", 
+    If:      "if", 
+    Else:    "else", 
+    Fun:     "fun",   // functions
+    Shape:   "shape", // shape X {}
+    Class:   "class", // class X {}
+    Fit:     "fit",   // fit X to Y {}
+    To:      "to", 
+    Fits:    "fits",    // class X fits Y {}
+    Extends: "extends", // shape X extends Y {}
+    Match:   "match",   // match x {}
+    Unit:    "unit",    // units and measures (nominal primitives)
+    Measure: "measure", // units and measures (nominal primitives)
+    Of:      "of"       // units and measures (nominal primitives)
+}
