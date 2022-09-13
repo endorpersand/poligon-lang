d14eaaa2f93ff017c1f901fb954a435c946d953f
endorpersand <daendrdragon@gmail.com>
2022-09-13 19:28:23 -0400

    add parse_from_file in lib rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
+use std::{fs, io};
+use std::path::Path;
 mod lexer;
+
+pub fn parse_from_file(fp: impl AsRef<Path>) -> io::Result<()> {
+    let source = fs::read_to_string(fp)?;
+
+    let tokens = lexer::tokenize(&source);
+    Ok(())
+}
 
 #[cfg(test)]
 mod tests {
